ec910388b0b5ff04bb34913a3bbafb16677778da
dcDalin <mcdalinoluoch@gmail.com>
2018-02-22 13:50:52 +0300

    feat(browser sync): live reloading with browser sync
    watch for file changes and reload the browser, set up a dev server

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,29 @@
 const gulp = require('gulp');
 // Requires the gulp-sass plugin
 const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
 
-gulp.task('sass', function(){
-    return gulp.src('app/scss/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('app/css'))
+gulp.task('sass', function() {
+    return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
+      .pipe(sass())
+      .pipe(gulp.dest('app/css'))
+      .pipe(browserSync.reload({
+        stream: true
+    }))
 });
 
 // Gulp watch syntax
-gulp.task('watch', function(){
+gulp.task('watch', ['browserSync', 'sass'], function (){
     gulp.watch('app/scss/**/*.scss', ['sass']); 
-    // Other watchers
+    // Reloads the browser whenever HTML or JS files change
+    gulp.watch('app/*.html', browserSync.reload); 
+    gulp.watch('app/js/**/*.js', browserSync.reload); 
+});
+
+gulp.task('browserSync', function(){
+   browserSync.init({
+       server: {
+           baseDir: 'app'
+       },
+   }) 
 });
