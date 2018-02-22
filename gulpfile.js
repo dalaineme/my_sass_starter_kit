61c45e52dae4ca24bf745328efb1ec854dd9c5e5
dcDalin <mcdalinoluoch@gmail.com>
2018-02-22 15:17:04 +0300

    feat(gulp-imagemin): optimize images using gulp-imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,9 @@ const useref = require('gulp-useref');
 const uglify = require('gulp-uglify');
 const gulpIf = require('gulp-if');
 const cssnano = require('gulp-cssnano');
+const imagemin = require('gulp-imagemin');
+const cache = require('gulp-cache');
+
 
 gulp.task('sass', function() {
     return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
@@ -47,6 +50,16 @@ gulp.task('useref', function(){
       // Minifies only if it's a CSS file
       .pipe(gulpIf('*.css', cssnano()))
       .pipe(gulp.dest('dist'))
+});
+
+// optimize images
+ggulp.task('images', function(){
+    return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+    // Caching images that ran through imagemin
+    .pipe(cache(imagemin({
+        interlaced: true
+      })))
+    .pipe(gulp.dest('dist/images'))
 });
 
 gulp.task('useref', function(){
